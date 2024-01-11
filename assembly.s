5cccd222ecc929965ee0c920276bfd456b5571ec
Guilherme Lazari <guilhermelcs@gmail.com>
2024-01-11 00:30:05 -0300

    Initial version of refactored assembly writter

--- assembly.s
+++ assembly.s
@@ -1,125 +1,14 @@
+
 global _start
 section .text
 _start:
-	call main
-end:
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+    call main
+    ; exit syscall
+    mov rax, 60
+    xor rdi, rdi ; exit code 0
+    syscall
+
 
 main:
-	mov rdi, 0    ; rdi is the offset for num_to_string
-	mov r12, 2    ; r12 is the number to check
-main_loop:
-	cmp r12, 200000
-	jg main_end_loop
-	mov rax, r12
-	call check_prime
-	cmp rax, 1
-	jne main_nextnum
-	mov rax, r12
-	call num_to_string
-	add rdi, rdx ; increments offset by length of number
-	cmp rdi, 10000
-	jl main_nextnum ; print only once buffer has 100 bytes
-	mov rsi, number
-	mov rdx, rdi
-	call print
-	mov rdi, 0   ; reset offset
-main_nextnum:
-	inc r12
-	jmp main_loop
-main_end_loop:
-	nop
-	cmp rdi, 0
-	je main_end
-	mov rsi, number
-	mov rdx, rdi
-	call print   ; print remaining numbers
-	mov rdi, 0   ; reset offset
-main_end:
-	nop
-	ret ; return to calling proceedure
-
-; prints message at address rsi with length rdx to stdout
-print:
-	mov rax, 1 ; system call for write
-	mov rdi, 1 ; file handle 1 is stdout
-	syscall
-	ret ; return to calling proceedure
-
-print_is_prime:
-	mov rsi, isprime
-	mov rdx, 9
-	call print
-	ret ; return to calling proceedure
-
-; converts number in rax to string in :number+rdi and returns address in rsi and length in rdx
-num_to_string:
-	mov r10, 0       ; r10 is the length of the number
-	mov rcx, rax     ; rcx is the number
-	mov r8, number   ; r8 is the start address of the number
-	add r8, rdi      ; r8 is the start address of the number + rdi
-num_to_string_loop:
-	mov rax, rcx
-	mov rdx, 0
-	mov rbx, 10
-	div rbx          ; rax = rax / rbx, rdx = rax % rbx
-	add rdx, '0'     ; convert to ascii
-	mov byte [r8 + r10], dl   ; store in number + rdi
-	inc r10          ; increment length
-	mov rcx, rax
-	cmp rax, 0
-	jne num_to_string_loop
-	mov rcx, r10     ; rcx will be the end pointer
-	dec rcx   ;  length is one less than end pointer
-	mov rsi, 0       ; rsi will be the start pointer
-num_to_string_reverse_loop:
-	nop
-	mov byte dl, [r8 + rsi]
-	mov byte al, [r8 + rcx]
-	mov byte [r8 + rsi], al
-	mov byte [r8 + rcx], dl
-	inc rsi
-	dec rcx
-	cmp rsi, rcx
-	jle num_to_string_reverse_loop
-	mov byte [r8 + r10], 10   ; add newline
-	inc r10          ; increment length
-	mov rsi, r8
-	mov rdx, r10
-	ret ; return to calling proceedure
-
-; checks if number in rax is prime, returns 1 if prime, 0 if not
-check_prime:
-	mov rbx, 2      ; rbx is the divisor
-	mov rcx, rax    ; rcx is the number
-check_prime_loop:
-	cmp rcx, rbx
-	jle check_prime_end_loop
-	mov eax, ecx
-	cdq ; extends number to rdx (with zeros)
-	div dword ebx         ; rax = rax / rbx, rdx = rax % rbx
-	inc rbx         ; increment divisor
-	cmp rdx, 0     ; if remainder is 0, number is not prime
-	jne check_prime_loop
-	mov rax, 0     ; number is not prime
-	ret
-check_prime_end_loop:
-	mov rax, 1     ; number is prime
-	ret
-	ret ; return to calling proceedure
-
-print_not_prime:
-	mov rsi, notprime
-	mov rdx, 10
-	call print
-	ret ; return to calling proceedure
-
-
-section .data
-isprime:	db	"is prime", 10
-notprime:	db	"not prime", 10
-
-section .bss
-number:	resb	10024
+	mov rdi, 10
+	ret ; return to calling proceedure from main
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,203 @@
 mod primes_v1;
 
+use std::fs::File;
+use std::io::prelude::*;
+
+struct Program {
+    procedures: Vec<Procedure>,
+}
+
+struct Procedure {
+    name: String,
+    instructions: Vec<Instructions>,
+    description: String,
+}
+
+enum Instructions {
+    Raw(String),
+    Label(String),
+    Loop(Loop1),
+    IfElse(IfElse1),
+    Call(String),
+}
+
+struct Loop1 {
+    exit_condition: JumpCondition,
+    instructions: Vec<Instructions>,
+}
+
+struct JumpCondition {
+    condition: String,
+    jump: String,
+}
+
+struct IfElse1 {
+    condition: JumpCondition,
+    if_instructions: Vec<Instructions>,
+    else_instructions: Vec<Instructions>,
+}
+
 fn main() {
+    let program = Program {
+        procedures: vec![Procedure {
+            name: "main".to_string(),
+            instructions: vec![
+                Instructions::Raw("mov rdi, 10".to_string()),
+            ],
+            description: "main function".to_string(),
+        }],
+    };
+    let assembly = CodeGenerator::new().generate_code(&program);
+    save_to_file("assembly.s", &assembly);
+    nasm("assembly.s", "assembly.o");
+    ld("assembly.o", "assembly");
     println!("Hello, world!");
+}
+
+fn save_to_file(filename: &str, contents: &str) {
+    let mut file = File::create(filename).expect("create failed");
+    file.write_all(contents.as_bytes()).expect("write failed");
+}
+
+struct CodeGenerator {
+    assembly: String,
+    lable_counter: usize,
+}
+
+impl CodeGenerator {
+    fn new() -> CodeGenerator {
+        CodeGenerator {
+            assembly: String::new(),
+            lable_counter: 0,
+        }
+    }
+
+    fn generate_code(&mut self, program: &Program) -> String {
+        self.assembly.push_str(
+            "
+global _start
+section .text
+_start:
+    call main
+    ; exit syscall
+    mov rax, 60
+    xor rdi, rdi ; exit code 0
+    syscall
+
+
+",
+        );
+
+        for procedure in &program.procedures {
+            self.generate_procedure(procedure);
+        }
+
+        self.assembly.clone()
+    }
+
+    fn generate_procedure(&mut self, procedure: &Procedure) {
+        self.assembly.push_str(&format!("{}:\n", procedure.name));
+
+        for instruction in &procedure.instructions {
+            self.generate_instruction(instruction);
+        }
+
+        // return to calling function
+        self.assembly.push_str(&format!(
+            "\tret ; return to calling proceedure from {}\n",
+            procedure.name
+        ));
+    }
+
+    fn generate_instruction(&mut self, instruction: &Instructions) {
+        match instruction {
+            Instructions::Raw(raw) => self.assembly.push_str(&format!("\t{}\n", raw)),
+            Instructions::Label(label) => self.assembly.push_str(&format!("{}:\n", label)),
+            Instructions::Loop(loop1) => self.generate_loop(loop1),
+            Instructions::IfElse(if_else) => self.generate_if_else(if_else),
+            Instructions::Call(call) => self.assembly.push_str(&format!("\tcall {}\n", call)),
+        }
+    }
+
+    fn generate_loop(&mut self, loop1: &Loop1) {
+        let loop_number = self.lable_counter;
+        self.lable_counter += 1;
+
+        self.assembly.push_str(&format!("loop_{}:\n", loop_number));
+
+        // add conditional jump
+        self.assembly
+            .push_str(&format!("\t{}\n", loop1.exit_condition.condition));
+        self.assembly.push_str(&format!(
+            "\t{} loop_{}_end",
+            loop1.exit_condition.jump, loop_number
+        ));
+
+        for instruction in &loop1.instructions {
+            self.generate_instruction(instruction);
+        }
+
+        self.assembly
+            .push_str(&format!("\tjmp loop_{}\n", loop_number));
+        self.assembly
+            .push_str(&format!("loop_{}_end:\n", loop_number));
+    }
+
+    fn generate_if_else(&mut self, if_else: &IfElse1) {
+        let if_else_number = self.lable_counter;
+        self.lable_counter += 1;
+
+        // add conditional jump
+        self.assembly
+            .push_str(&format!("\t{}\n", if_else.condition.condition));
+        self.assembly.push_str(&format!(
+            "\t{} if_else_{}_else",
+            if_else.condition.jump, if_else_number
+        ));
+
+        for instruction in &if_else.if_instructions {
+            self.generate_instruction(instruction);
+        }
+
+        self.assembly
+            .push_str(&format!("\tjmp if_else_{}_end\n", if_else_number));
+        self.assembly
+            .push_str(&format!("if_else_{}_else:\n", if_else_number));
+
+        for instruction in &if_else.else_instructions {
+            self.generate_instruction(instruction);
+        }
+
+        self.assembly
+            .push_str(&format!("if_else_{}_end:\n", if_else_number));
+    }
+}
+
+fn ld(infile: &str, outfile: &str) {
+    let output = std::process::Command::new("ld")
+        .arg("-o")
+        .arg(outfile)
+        .arg(infile)
+        .output()
+        .expect("failed to execute process");
+    if !output.status.success() {
+        println!("{:?}", output);
+        panic!("ld failed");
+    }
+}
+
+fn nasm(infile: &str, outfile: &str) {
+    let output = std::process::Command::new("nasm")
+        .arg("-f")
+        .arg("elf64")
+        .arg("-o")
+        .arg(outfile)
+        .arg(infile)
+        .output()
+        .expect("failed to execute process");
+    if !output.status.success() {
+        println!("{:?}", output);
+        println!("{}", String::from_utf8_lossy(&output.stderr));
+        panic!("nasm failed");
+    }
 }
