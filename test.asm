92c10ea53ae82414594e510c86b180eb2c2b6d6b
Guilherme Lazari <guilhermelcs@gmail.com>
2024-02-09 18:33:20 -0300

    First N primes in mylang!!!

--- test.asm
+++ test.asm
@@ -15,51 +15,112 @@ main:
 	mov rbp, rsp
 	sub rsp, 16
 	; body
-	mov rax, 50
+	mov rax, 2
 	mov [rbp - 8], rax
-	mov rax, 987654123
+	mov rax, 0
+	mov [rbp - 16], rax
+while_condition_0:
+	mov rax, [rbp - 8]
+	push rax
+	mov rax, 100
+	pop rbx
+	cmp rbx, rax
+	jge while_end_0
+while_body_0:
+if_condition_1:
+	mov rax, [rbp - 8]
 	push rax
 	sub rsp, 8
-	call print_number
+	call is_prime
+	mov rax, [rsp]
+	add rsp, 16
+	push rax
+	mov rax, 1
+	pop rbx
+	cmp rbx, rax
+	jne else_1
+if_body_1:
+	mov rax, [rbp - 8]
+	push rax
+	sub rsp, 8
+	call print_numberln
 	mov rax, [rsp]
 	add rsp, 16
 	mov [rbp - 16], rax
-	mov rax, 10
+	jmp end_1
+else_1:
+end_1:
+	mov rax, [rbp - 8]
 	push rax
-	sub rsp, 8
-	call print_chrln
-	mov rax, [rsp]
-	add rsp, 16
-	mov [rbp - 16], rax
+	mov rax, 1
+	mov rbx, rax
+	pop rax
+	add rax, rbx
+	mov [rbp - 8], rax
+	jmp while_condition_0
+while_end_0:
 	; epilogue
 	add rsp, 16
 	pop rbp
 	ret
-print_numberln:
+is_prime:
 	; prologue
 	push rbp
 	mov rbp, rsp
 	sub rsp, 8
 	; body
+	mov rax, 2
+	mov [rbp - 8], rax
+while_condition_2:
+	mov rax, [rbp - 8]
+	push rax
+	mov rax, [rbp - -24]
+	pop rbx
+	cmp rbx, rax
+	jge while_end_2
+while_body_2:
+if_condition_3:
 	mov rax, [rbp - -24]
 	push rax
-	sub rsp, 8
-	call print_number
-	mov rax, [rsp]
-	add rsp, 16
-	mov [rbp - 8], rax
-	mov rax, 10
+	mov rax, [rbp - 8]
+	mov rbx, rax
+	pop rax
+	cdq
+	div rbx
+	mov rax, rdx
 	push rax
-	sub rsp, 8
-	call print_chr
-	mov rax, [rsp]
-	add rsp, 16
+	mov rax, 0
+	pop rbx
+	cmp rbx, rax
+	jne else_3
+if_body_3:
+	mov rax, 0
+	mov [rbp + 16], rax
+	add rsp, 8
+	pop rbp
+	ret
+	jmp end_3
+else_3:
+end_3:
+	mov rax, [rbp - 8]
+	push rax
+	mov rax, 1
+	mov rbx, rax
+	pop rax
+	add rax, rbx
 	mov [rbp - 8], rax
+	jmp while_condition_2
+while_end_2:
+	mov rax, 1
+	mov [rbp + 16], rax
+	add rsp, 8
+	pop rbp
+	ret
 	; epilogue
 	add rsp, 8
 	pop rbp
 	ret
-print_number:
+print_numberln:
 	; prologue
 	push rbp
 	mov rbp, rsp
@@ -69,14 +130,14 @@ print_number:
 	mov [rbp - 8], rax
 	mov rax, [rbp - -24]
 	mov [rbp - 16], rax
-while_condition_0:
+while_condition_4:
 	mov rax, [rbp - 16]
 	push rax
 	mov rax, 0
 	pop rbx
 	cmp rbx, rax
-	jle while_end_0
-while_body_0:
+	jle while_end_4
+while_body_4:
 	mov rax, [rbp - 16]
 	push rax
 	mov rax, 10
@@ -92,20 +153,20 @@ while_body_0:
 	pop rax
 	add rax, rbx
 	mov [rbp - 8], rax
-	jmp while_condition_0
-while_end_0:
+	jmp while_condition_4
+while_end_4:
 	mov rax, 0
 	mov [rbp - 24], rax
 	mov rax, 0
 	mov [rbp - 32], rax
-while_condition_1:
+while_condition_5:
 	mov rax, [rbp - -24]
 	push rax
 	mov rax, 0
 	pop rbx
 	cmp rbx, rax
-	jle while_end_1
-while_body_1:
+	jle while_end_5
+while_body_5:
 	mov rax, 10
 	push rax
 	mov rax, [rbp - 8]
@@ -127,11 +188,16 @@ while_body_1:
 	pop rax
 	cdq
 	div rbx
+	push rax
+	mov rax, 48
+	mov rbx, rax
+	pop rax
+	add rax, rbx
 	mov [rbp - 24], rax
 	mov rax, [rbp - 24]
 	push rax
 	sub rsp, 8
-	call print_digit
+	call print_chr
 	mov rax, [rsp]
 	add rsp, 16
 	mov [rbp - 16], rax
@@ -151,8 +217,15 @@ while_body_1:
 	pop rax
 	sub rax, rbx
 	mov [rbp - 8], rax
-	jmp while_condition_1
-while_end_1:
+	jmp while_condition_5
+while_end_5:
+	mov rax, 10
+	push rax
+	sub rsp, 8
+	call print_chr
+	mov rax, [rsp]
+	add rsp, 16
+	mov [rbp - 16], rax
 	; epilogue
 	add rsp, 32
 	pop rbp
@@ -165,14 +238,14 @@ pow:
 	; body
 	mov rax, 1
 	mov [rbp - 8], rax
-while_condition_2:
+while_condition_6:
 	mov rax, [rbp - -24]
 	push rax
 	mov rax, 0
 	pop rbx
 	cmp rbx, rax
-	jle while_end_2
-while_body_2:
+	jle while_end_6
+while_body_6:
 	mov rax, [rbp - 8]
 	push rax
 	mov rax, [rbp - -32]
@@ -187,61 +260,13 @@ while_body_2:
 	pop rax
 	sub rax, rbx
 	mov [rbp - -24], rax
-	jmp while_condition_2
-while_end_2:
+	jmp while_condition_6
+while_end_6:
 	mov rax, [rbp - 8]
 	mov [rbp + 16], rax
 	add rsp, 8
 	pop rbp
 	ret
-	; epilogue
-	add rsp, 8
-	pop rbp
-	ret
-print_digit:
-	; prologue
-	push rbp
-	mov rbp, rsp
-	sub rsp, 16
-	; body
-	mov rax, [rbp - -24]
-	push rax
-	mov rax, 48
-	mov rbx, rax
-	pop rax
-	add rax, rbx
-	mov [rbp - 8], rax
-	mov rax, [rbp - 8]
-	push rax
-	sub rsp, 8
-	call print_chr
-	mov rax, [rsp]
-	add rsp, 16
-	mov [rbp - 16], rax
-	; epilogue
-	add rsp, 16
-	pop rbp
-	ret
-print_chrln:
-	; prologue
-	push rbp
-	mov rbp, rsp
-	sub rsp, 8
-	; body
-	mov rax, [rbp - -24]
-	push rax
-	sub rsp, 8
-	call print_chr
-	mov rax, [rsp]
-	add rsp, 16
-	mov [rbp - 8], rax
-	mov rax, 10
-	push rax
-	sub rsp, 8
-	call print_chr
-	mov rax, [rsp]
-	add rsp, 16
-	mov [rbp - 8], rax
 	; epilogue
 	add rsp, 8
 	pop rbp
@@ -260,86 +285,5 @@ print_chr:
 	syscall
 	; epilogue
 	add rsp, 0
-	pop rbp
-	ret
-add_all:
-	; prologue
-	push rbp
-	mov rbp, rsp
-	sub rsp, 16
-	; body
-	mov rax, 0
-	mov [rbp - 8], rax
-	mov rax, 1
-	mov [rbp - 16], rax
-while_condition_3:
-	mov rax, [rbp - 16]
-	push rax
-	mov rax, [rbp - -24]
-	pop rbx
-	cmp rbx, rax
-	jge while_end_3
-while_body_3:
-	mov rax, [rbp - 8]
-	push rax
-	mov rax, [rbp - 16]
-	mov rbx, rax
-	pop rax
-	add rax, rbx
-	mov [rbp - 8], rax
-	mov rax, [rbp - 16]
-	push rax
-	mov rax, 1
-	mov rbx, rax
-	pop rax
-	add rax, rbx
-	mov [rbp - 16], rax
-	jmp while_condition_3
-while_end_3:
-	mov rax, [rbp - 8]
-	mov [rbp + 16], rax
-	add rsp, 16
-	pop rbp
-	ret
-	; epilogue
-	add rsp, 16
-	pop rbp
-	ret
-add_all_do_while:
-	; prologue
-	push rbp
-	mov rbp, rsp
-	sub rsp, 16
-	; body
-	mov rax, 0
-	mov [rbp - 8], rax
-	mov rax, 1
-	mov [rbp - 16], rax
-do_while_body_4:
-	mov rax, [rbp - 8]
-	push rax
-	mov rax, [rbp - 16]
-	mov rbx, rax
-	pop rax
-	add rax, rbx
-	mov [rbp - 8], rax
-	mov rax, [rbp - 16]
-	push rax
-	mov rax, 1
-	mov rbx, rax
-	pop rax
-	add rax, rbx
-	mov [rbp - 16], rax
-do_while_condition_4:
-	mov rax, [rbp - 16]
-	push rax
-	mov rax, [rbp - -24]
-	pop rbx
-	cmp rbx, rax
-	jl do_while_end_4
-	jmp do_while_body_4
-do_while_end_4:
-	; epilogue
-	add rsp, 16
 	pop rbp
 	ret
